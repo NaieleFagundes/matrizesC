a70a4dc1d846bb9a85822a20ad737b430514f1ef
Naiele Fagundes <163013181+NaieleFagundes@users.noreply.github.com>
2025-07-13 15:18:36 -0300

    Add files via upload

---
+++ ex10M.c
@@ -0,0 +1,31 @@
+//Lê uma matriz de 3 x 3 elementos. No final, calcula a soma dos elementos que estão na diagonal secundaria.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int mat [3][3];
+    int soma=0, linha, coluna;
+
+    for (linha = 0; linha < 3 ; linha++) {
+        for (coluna = 0; coluna < 3; coluna++){
+            printf ("Digite o elemento para a matriz: \n");
+            scanf ("%d", &mat[linha][coluna]);
+
+        }
+    }
+    
+
+    for (linha = 0; linha < 3; linha++){
+       for ( coluna= 0; coluna < 3; coluna++){
+           if ((linha + coluna)==2){
+               soma += mat [linha][coluna];
+           } 
+           printf ("%d ", mat [linha][coluna]);
+       }
+       printf (" \n");
+    }
+   printf ("A soma é %d", soma);
+   return 0;
+}
