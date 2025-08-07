925a362f7f54317eefcfb860df59a5b453122e9a
Naiele Fagundes <163013181+NaieleFagundes@users.noreply.github.com>
2025-08-06 21:08:47 -0300

    Add files via upload

---
+++ Matriz Diagonal Principal com função rand.c
@@ -0,0 +1,30 @@
+//  Leia uma matriz de 4 x 4 elementos.
+//Preencha a matriz com dados aleatóres o e calcule a soma dos elementos que estao
+//abaixo da diagonal principal.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int mat [4][4], i, j, soma =0;
+    
+    for (i = 0; i < 4; i++){
+         for (j = 0; j < 4; j++){
+            mat [i][j]= rand()%100;
+        }
+    }
+    
+    for (i = 0; i < 4; i++){
+        for (j = 0; j < 4; j++){
+           if (i > j){
+              soma+= mat [i][j];
+            }
+        }
+        printf ("\n");    
+    }
+   
+   
+    printf ("A soma dos elementos da Diagonal Principal é %d ",soma);
+    return 0;
+}
